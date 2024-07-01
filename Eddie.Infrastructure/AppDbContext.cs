b7255784ab24a7c19b1e1556bb8a3eaab990db63
Yan_Brock <yanbrock@protonmail.com>
2024-07-01 19:59:23 +0200

    Implemented: base Infrastructure layer with Context class

---
+++ Eddie.Infrastructure/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Eddie.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Eddie.Infrastructure;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; }
+}
